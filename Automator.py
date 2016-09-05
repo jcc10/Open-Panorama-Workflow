39887cd817cd7e0d5d85d96e6604efe246f41888
Joshua Cline <wwjdtd@gmail.com>
2016-09-05 12:24:34 -0700

    Should be done with auto-rotate-to-Nadir function
    Let's see if this works...

--- Automator.py
+++ Automator.py
@@ -35,6 +35,24 @@ class PanoAutomator :
                     out.append(s2)
             return out
 
+    # This generates the rotation script used by nona and saves it to TepScript.pto so the program can use it. Breakdown below...
+    #
+    # p f2 w#Width h#Height v360 E0 R0 n"TIFF_m c:NONE r:CROP"
+    # This is the output format. #Width and #Height are obtained from the source image (We are just rotating the pre-exsisting image, so this works)
+    # m g1 i0 f0 m2 p0.00784314
+    # I honestly don't know what this does but I'm too afraid to remove it, so it stays.
+    #
+    # i w#Width h#Height f4 v360 r#Rotation p#Pitch y#Yaw n".\Logo\360_0162 Auto-CP.1-2.tif"
+    # This is the source image values #Width and # Height should be obvious
+    # #Rotation, #Pitch, and #Yaw are how the image will be rotated in the final product.
+    # (To get both top and bottom of the image visable use 90, 0, 90 repectivly)
+    def generate_script(self, mode, filename, Height, Width):
+        if mode == 0 :
+            script = 'p f2 w' + Width + ' h' + Height + ' v360 E0 R0 n"TIFF_m c:NONE r:CROP"\nm g1 i0 f0 m2 p0.00784314\n\ni w' + Width + ' h' + Height + ' f4 v360 r0 p90 y0 n".\\Stitched\\' + filename +'"'
+        elif mode == 1 :
+            script = 'p f2 w' + Width + ' h' + Height + ' v360 E0 R0 n"TIFF_m c:NONE r:CROP"\nm g1 i0 f0 m2 p0.00784314\n\ni w' + Width + ' h' + Height + ' f4 v360 r0 p-90 y0 n".\\Logo\\' + filename +'"'
+        return script
+
     # Used to split a image in half using ImageMagick, To be upgraded in the near future.
     def autoingest(self):
         inputFiles = self.folderlist('Ingest')
@@ -53,6 +71,37 @@ class PanoAutomator :
             self.command('convert "./Ingest/' + F + '" -crop 3888x3888+0+0 "./Ingested/' + FoutL + '"')
             self.log('Left done...')
         self.log('All files done')
+
+    # This rotates the panorama you made so you can edit the Nadir
+    def autoRotateToNadir(self):
+        inputFiles = self.folderlist('Stitched')
+        inputFiles2 = self.namestrip(inputFiles, 4)
+        outputFiles = self.folderlist('Nadir')
+        todoFiles = list(set(inputFiles2) - set(outputFiles))
+        for F in todoFiles :
+            self.log('Processing image ' + F + ' information...')
+            origWidth, origHeight = Image.open(os.getcwd() + "\\Stitched\\" + F).size
+            origHeight = str(origHeight)
+            origWidth = str(origWidth)
+            scriptText = self.generate_script(0,F,origHeight,origWidth)
+            # We now write the script in the function so we *should* never have them end up in diffrent places.
+            wr = open('.\\TempScript.pto', 'w')
+            wr.write(scriptText)
+            self.log("Image info obtained. Rotating...")
+            self.command('nona -o ".\\Nadir\\' + F + '" .\\TempScript.pto')
+            self.log('Image ' + F + ' Rotated.')
+        self.remove0000('Nadir')
+
+    # This removes the 0000.tif that is automaitcally added to the files when rotated
+    # Might want to move into namestrip (Since it is kinda striping the name.
+    def remove0000(self, Folder):
+        print('Renaming files so there is not a 0000.tif at the end')
+        files = folderlist(Folder)
+        for F in files:
+            if F[-8:] == '0000.tif':
+                newname = F.replace('0000.tif', '')
+                os.rename('.\\' + Folder + '\\' + F,'.\\' + Folder + '\\' + newname)
+        print('Files renamed.')
 
 
 
@@ -104,33 +153,6 @@ def namestrip(data, mode=0):
                 o.append(s)
         return o
 
-# This rotates the panorama you made so you can edit the Nadir
-def autoRotateToNadir():
-    inputFiles = folderlist('Stitched')
-    inputFiles2 = namestrip(inputFiles, 4)
-    outputFiles = folderlist('Nadir')
-    todoFiles = list(set(inputFiles2) - set(outputFiles))
-    for F in todoFiles :
-        print('Processing image ' + F + ' information...')
-        origWidth, origHeight = Image.open(os.getcwd() + "\\Stitched\\" + F).size
-        origHeight = str(origHeight)
-        origWidth = str(origWidth)
-        generate_script(0,F,origHeight,origWidth)
-        print("Image info obtained. Rotating...")
-        os.system('nona -o ".\\Nadir\\' + F + '" .\\TempScript.pto')
-        print('Image ' + F + ' Rotated.')
-    remove0000('Nadir')
-
-# This removes the 0000.tif that is automaitcally added to the files when rotated
-def remove0000(Folder):
-    print('Renaming files so there is not a 0000.tif at the end')
-    files = folderlist(Folder)
-    for F in files:
-        if F[-8:] == '0000.tif':
-            newname = F.replace('0000.tif', '')
-            os.rename('.\\' + Folder + '\\' + F,'.\\' + Folder + '\\' + newname)
-    print('Files renamed.')
-
 # This adds the logo, it does not always work correctly due to alignment issues.
 def addLogo(LogoFile):
     print('Adding logos...')
@@ -172,32 +194,11 @@ def convert():
         os.system('convert -quiet ".\\Final\\tif\\' + F + '.tif" ".\\final\\jpg\\' + F + '.jpg"')
     print("All Converts Complete...")
 
-# This generates the rotation script used by nona and saves it to TepScript.pto so the program can use it. Breakdown below...
-#
-# p f2 w#Width h#Height v360 E0 R0 n"TIFF_m c:NONE r:CROP"
-# This is the output format. #Width and #Height are obtained from the source image (We are just rotating the pre-exsisting image, so this works)
-# m g1 i0 f0 m2 p0.00784314
-# I honestly don't know what this does but I'm too afraid to remove it, so it stays.
-#
-# i w#Width h#Height f4 v360 r#Rotation p#Pitch y#Yaw n".\Logo\360_0162 Auto-CP.1-2.tif"
-# This is the source image values #Width and # Height should be obvious
-# #Rotation, #Pitch, and #Yaw are how the image will be rotated in the final product.
-# (To get both top and bottom of the image visable use 90, 0, 90 repectivly)
-def generate_script(mode, filename, Height, Width):
-    if mode == 0 :
-        text = 'p f2 w' + Width + ' h' + Height + ' v360 E0 R0 n"TIFF_m c:NONE r:CROP"\nm g1 i0 f0 m2 p0.00784314\n\ni w' + Width + ' h' + Height + ' f4 v360 r0 p90 y0 n".\\Stitched\\' + filename +'"'
-    elif mode == 1 :
-        text = 'p f2 w' + Width + ' h' + Height + ' v360 E0 R0 n"TIFF_m c:NONE r:CROP"\nm g1 i0 f0 m2 p0.00784314\n\ni w' + Width + ' h' + Height + ' f4 v360 r0 p-90 y0 n".\\Logo\\' + filename +'"'
-    wr = open('.\\TempScript.pto', 'w')
-    wr.write(text)
-
-
 # This just calls all the functions in order, I will eventually make a menu for this but for now, here you are.
 #Let's see if the new object works...
 PA = PanoAutomator()
 PA.autoingest()
-#autoingest()
-#autoRotateToNadir()
+PA.autoRotateToNadir()
 # You can change what the logo file name is here, I don't remember if I ever fixed the bug where you can't have spaces or not...
 #addLogo('Advanced_Nadir_small.tif')
 #autoRotateFromNadir()
