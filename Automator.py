a6c9994f173501f645bca01586343c61525c5cc2
Joshua Cline <wwjdtd@gmail.com>
2016-09-13 21:50:00 -0700

    I'm adding in log levels and accepted file types.

--- Automator.py
+++ Automator.py
@@ -8,8 +8,18 @@
 import Code
 
 # This just calls all the functions in order, I will eventually make a menu for this but for now, here you are.
-#Let's see if the new object works...
-PA = Code.PanoAutomator()
+FileExtentions = ["jpg","jpeg","png","tif","tiff"]
+# File Extentions that will get prosessed, if it's not on this list it won't even get to the file splitters.
+logLevel = 2
+# Log Levels
+# 0 = Silent, No text whats so ever
+#     Eventually this will also surpress errors.
+# 1 = Shows Every section it's prosessing
+# 2 = Shows Every file it's prosessing
+# 3 = Shows Every major step in the file it's prossesing
+# 4 = Reserved for future use, acts like 3
+# 5 = Shows Debug Text
+PA = Code.PanoAutomator(0)
 PA.splitLeftRight('Ingest', 'Ingested')
 # Rotate the image so the nadir is in the center
 PA.rotateEquirectangular('Stitched', 'Nadir', 0, 90, 0)
@@ -19,4 +29,7 @@ PA.addLogo('Nadir', 'Logo', 'Advanced_Nadir_small.tif')
 PA.rotateEquirectangular('Logo', 'Final/tif', 0, -90, 0)
 PA.convert('Final/tif', 'tif', 'Final/jpg', 'jpg')
 # This is for if you just click on the program and it actually runs. (So it dosent just close out.) The wrapper is B/C I was debugging it and am still doing so.
-input("Press enter to close")
+if logLevel >= 1 :
+    input("Press enter to close")
+else:
+    None
